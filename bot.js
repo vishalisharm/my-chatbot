e71fd7009eba902d18b6539cc078f6c9df4d4f9a
vishalisharm <131683797+vishalisharm@users.noreply.github.com>
2023-11-14 21:48:40 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const userInputElement = document.getElementById("user-input");
   const sendButton = document.getElementById("send-button");
 
+  userInputElement.addEventListener("keyup", function (event) {
+    // Check if the pressed key is Enter (key code 13)
+    if (event.key === "Enter") {
+      const userResponse = userInputElement.value.trim().toLowerCase();
+      handleUserInput(userResponse);
+    }
+  });
+
+
   if (!conversationElement || !userInputElement || !sendButton) {
     console.error("One or more required HTML elements not found.");
     return;
@@ -35,17 +44,34 @@ document.addEventListener("DOMContentLoaded", function () {
   function displayOptions(options) {
     const optionsContainer = document.createElement("div");
     optionsContainer.classList.add("options");
-
-    Object.keys(options).forEach((option) => {
+  
+    Object.entries(options).forEach(([option, target]) => {
       const optionButton = document.createElement("button");
       optionButton.classList.add("option");
-      optionButton.textContent = option;
-      optionButton.addEventListener("click", function () {
-        handleUserInput(option);
-      });
+  
+      if (target.startsWith("http")) {
+        // If the target starts with "http", it's a link
+        const linkElement = document.createElement("a");
+        linkElement.href = target;
+        linkElement.textContent = option;
+        optionButton.appendChild(linkElement);
+      } else if (target.startsWith("[")) {
+        // If the target starts with "[", it's a Markdown-style link
+        const linkText = target.substring(1, target.length - 1);
+        const linkElement = document.createElement("a");
+        linkElement.href = linkText;
+        linkElement.textContent = option;
+        optionButton.appendChild(linkElement);
+      } else {
+        optionButton.textContent = option;
+        optionButton.addEventListener("click", function () {
+          handleUserInput(option);
+        });
+      }
+  
       optionsContainer.appendChild(optionButton);
     });
-
+  
     if (conversationState !== "start") {
       const restartButton = document.createElement("button");
       restartButton.classList.add("option");
@@ -55,9 +81,10 @@ document.addEventListener("DOMContentLoaded", function () {
       });
       optionsContainer.appendChild(restartButton);
     }
-
+  
     conversationElement.appendChild(optionsContainer);
   }
+  
 
   function restartConversation() {
     conversationState = "start";
@@ -76,7 +103,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (!userName) {
       userName = userResponse;
-      displayMessage(`MedicineBot: Nice to meet you, ${userName}! What symptoms are you facing?`, "bot");
+      displayMessage(`MedBot: Nice to meet you, ${userName}! What symptoms are you facing?`, "bot");
       conversationState = "user_name";
       displayOptions(decisionTree[conversationState].options);
     } else {
@@ -91,7 +118,7 @@ document.addEventListener("DOMContentLoaded", function () {
           displayOptions(decisionTree[conversationState].options);
         }
       } else {
-        displayMessage("Bot: I'm not sure. Please provide a valid response.", "bot");
+        displayMessage("MedBot: I'm not sure. Please provide a valid response.", "bot");
       }
     }
 
